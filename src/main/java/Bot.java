04451a737b6d23d5e485672e4adfd2ec26ba4198
Андрей Григорьев <greegav@gmail.com>
2019-07-29 18:44:57 +0300

    1

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -4,6 +4,10 @@ import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
 public class Bot extends TelegramLongPollingBot {
 
     private void sendMsg(Message message, String default_text) {
@@ -34,12 +38,48 @@ public class Bot extends TelegramLongPollingBot {
                     showTestMessage(message);
                     break;
                 }
+                case "/time": {
+                    Date currentDate = new Date();
+                    long chat_id = update.getMessage().getChatId();
+                    String messageSend = currentDate.toString();
+                    SendMessage messg = new SendMessage().setChatId(chat_id).setText(messageSend);
+                    try {
+                        execute(messg); // Sending our message object to user
+                    } catch (TelegramApiException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                }
+//////
+                case "/ident": {
+                    String sendtext = "Ident:\n";
+                    sendtext += "Chat ID: " + update.getMessage().getChatId() + "(" + update.getMessage().getFrom().getUserName() + ")";
+                    System.out.println(sendtext);
+                    sendtext = "Idented.\n";
+//                    sendtext+="update.getMessage().getEntities(): "+update.getMessage().getEntities()+"\n\n";
+//                    sendtext+="update.getMessage().getFrom(): "+update.getMessage().getFrom()+"\n\n";
+//                    sendtext+="update.getMessage(): "+update.getMessage()+"\n\n";
+                    sendtext += "update.toString(): " + update.toString() + "\n\n";
+                    System.out.println(sendtext);
+                    List<String> sentence = Arrays.asList(update.toString().split("\\{"));
+                    for (String str : sentence) {
+                        System.out.println("{"+str);
+                        sendtext += str + "\n\n";
+                    }
+                    sendMsg(message, sendtext);
+                    break;
+                }
 
+//////
                 default: {
-                    showDefaultMessage(message);
+                    System.out.println("Default");
+                    sendMsg(message, message.getText());
+                    System.out.println(message.getText());
                     break;
                 }
             }
+        } else {
+            System.out.println("message = null | !message.hasText()");
         }
     }
 
