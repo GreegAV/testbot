1d4fbc617b3cca5dab8a82de02668625a8e4b60d
Андрей Григорьев <greegav@gmail.com>
2019-07-30 18:33:54 +0300

    Full copy of OBRTLogbot. Started.

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,14 +1,98 @@
+import Staff.Service;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
 public class Bot extends TelegramLongPollingBot {
+    // По правильному, это надо было сделать отдельной веткой в гитхабе, но так сложилось :(
+
+//    public void onUpdateReceived(Update update) {
+//        update.getUpdateId();
+//        SendMessage sendMessage = new SendMessage().setChatId(update.getMessage().getChatId());
+//
+//        String msgToBot = update.getMessage().getText();
+//        System.out.println(msgToBot);
+//
+//        if (msgToBot.equals("Hi!")) {
+//            sendMessage.setText("И тебе Хай!");
+//            try {
+//                execute(sendMessage);
+//            } catch (TelegramApiException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//
+////        if (msgToBot.equals("@Счет".toLowerCase()) || msgToBot.equals("@Счёт".toLowerCase())) {
+////            sendMessage.setText("Кому счет?");
+////            try {
+////                execute(sendMessage);
+////            } catch (TelegramApiException e) {
+////                e.printStackTrace();
+////            }
+////        }
+//
+//        if (msgToBot.equals("@ident")) {
+//            String sendtext = "Ident:\n";
+//            sendtext += "Chat ID: " + update.getMessage().getChatId();
+//            sendtext += "\n";
+//            String getFirstName = update.getMessage().getFrom().getFirstName();
+//            String getLastName = update.getMessage().getFrom().getLastName();
+//            String getUserName = update.getMessage().getFrom().getUserName();
+//            if (getFirstName != null) {
+//                sendtext += "update.getMessage().getFrom().getFirstName(): " + getFirstName;
+//                sendtext += "\n";
+//            }
+//            if (getUserName != null) {
+//                sendtext += "update.getMessage().getFrom().getLastName(): " + getLastName;
+//                sendtext += "\n";
+//            }
+//            if (getUserName != null) {
+//                sendtext += "update.getMessage().getFrom().getUserName(): " + getUserName;
+//                sendtext += "\n";
+//            }
+////            System.out.println(sendtext);
+//            sendtext = "Idented.";
+//            sendMessage.setText(sendtext);
+//            System.out.println(update.getMessage());
+//            try {
+//                execute(sendMessage);
+//            } catch (TelegramApiException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//        ///////////////////// Finally log to sheets
+//        logToSheets(update);
+//
+//    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        Message message = update.getMessage();
+
+        if (message != null & message.hasText()) {
+            logToSheets(update);
+            if (message.getText().toLowerCase().equals("/time")) {
+                Date currentDate = new Date();
+                long chat_id = update.getMessage().getChatId();
+                String messageSend = currentDate.toString();
+                SendMessage messg = new SendMessage().setChatId(chat_id).setText(messageSend);
+                try {
+                    execute(messg); // Sending our message object to user
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 
     private void sendMsg(Message message, String default_text) {
         SendMessage sendMessage = new SendMessage();
@@ -24,75 +108,40 @@ public class Bot extends TelegramLongPollingBot {
         }
     }
 
-    @Override
-    public void onUpdateReceived(Update update) {
-        Message message = update.getMessage();
+    private void logToSheets(Update update) {
+        System.out.println("logging...");
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
+        String date = dateFormat.format(new Date(update.getMessage().getDate() * 1000L));
 
-        if (message != null & message.hasText()) {
-            switch (message.getText().toLowerCase()) {
-                case "/help": {
-                    showHelpMessage(message);
-                    break;
-                }
-                case "/test": {
-                    showTestMessage(message);
-                    break;
-                }
-                case "/time": {
-                    Date currentDate = new Date();
-                    long chat_id = update.getMessage().getChatId();
-                    String messageSend = currentDate.toString();
-                    SendMessage messg = new SendMessage().setChatId(chat_id).setText(messageSend);
-                    try {
-                        execute(messg); // Sending our message object to user
-                    } catch (TelegramApiException e) {
-                        e.printStackTrace();
-                    }
-                    break;
-                }
-//////
-                case "/ident": {
-                    String sendtext = "Ident:\n";
-                    sendtext += "Chat ID: " + update.getMessage().getChatId() + "(" + update.getMessage().getFrom().getUserName() + ")";
-                    System.out.println(sendtext);
-                    sendtext = "Idented.\n";
-//                    sendtext+="update.getMessage().getEntities(): "+update.getMessage().getEntities()+"\n\n";
-//                    sendtext+="update.getMessage().getFrom(): "+update.getMessage().getFrom()+"\n\n";
-//                    sendtext+="update.getMessage(): "+update.getMessage()+"\n\n";
-                    sendtext += "update.toString(): " + update.toString() + "\n\n";
-                    System.out.println(sendtext);
-                    List<String> sentence = Arrays.asList(update.toString().split("\\{"));
-                    for (String str : sentence) {
-                        System.out.println("{"+str);
-                        sendtext += str + "\n\n";
-                    }
-                    sendMsg(message, sendtext);
-                    break;
-                }
-
-//////
-                default: {
-                    System.out.println("Default");
-                    sendMsg(message, message.getText());
-                    System.out.println(message.getText());
-                    break;
-                }
-            }
-        } else {
-            System.out.println("message = null | !message.hasText()");
+        long userID = update.getMessage().getChatId();
+        String sheetName = "General";
+        String userName = "";
+//        if (userID == 221816696) {
+//            sheetName = "Test";
+//        }
+//        if (userID == 148370030) {
+//            sheetName = "Батова";
+//        }
+//        if (userID == 548498472) {
+//            sheetName = "Росочинская";
+//        }
+        if (sheetName.equals("General")) {
+            if (update.getMessage().getFrom().getFirstName() != null)
+                userName = update.getMessage().getFrom().getFirstName();
+            if (update.getMessage().getFrom().getLastName() != null)
+                userName += "." + update.getMessage().getFrom().getLastName();
+            if (update.getMessage().getFrom().getUserName() != null)
+                userName += "." + update.getMessage().getFrom().getUserName();
         }
-    }
+        String textFromBot = date + " " + userName + " " + update.getMessage().getText();
+        List<String> sentence = Arrays.asList(textFromBot.split(" "));
 
-    private void showDefaultMessage(Message message) {
-        sendMsg(message, "Default text");
-    }
+        try {
 
-    private void showTestMessage(Message message) {
-        sendMsg(message, "Test passed");
-    }
-
-    private void showHelpMessage(Message message) {
-        sendMsg(message, "Help!");
+            Service.writeToSheet(Service.getSheetsService(), sentence, sheetName);
+        } catch (IOException | GeneralSecurityException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
