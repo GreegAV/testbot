b61783a220aba6a80ec6a6b6b5f083fbac56f2fd
Андрей Григорьев <greegav@gmail.com>
2019-08-09 13:40:44 +0300

    Extracted Google Tools

--- src/main/java/com/obrttestbot/Service.java
+++ src/main/java/com/obrttestbot/Service.java
@@ -1,22 +1,11 @@
 package com.obrttestbot;
 
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.sheets.v4.Sheets;
-import com.google.api.services.sheets.v4.SheetsScopes;
 import com.google.api.services.sheets.v4.model.AppendValuesResponse;
 import com.google.api.services.sheets.v4.model.ValueRange;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
@@ -27,36 +16,8 @@ import java.util.List;
 public class Service {
     private static Sheets sheetsService;
 
-    private static Credential authorize() throws IOException, GeneralSecurityException {
-        InputStream in = Service.class.getResourceAsStream("/client_secrets.json");
-        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(
-                JacksonFactory.getDefaultInstance(), new InputStreamReader(in)
-        );
-
-        List<String> scopes = Arrays.asList(SheetsScopes.SPREADSHEETS);
-
-        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-                GoogleNetHttpTransport.newTrustedTransport(), JacksonFactory.getDefaultInstance(),
-                clientSecrets, scopes)
-                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File("tokens")))
-                .setAccessType("offline")
-                .build();
-
-        return new AuthorizationCodeInstalledApp(
-                flow, new LocalServerReceiver())
-                .authorize("user");
-    }
-
-    public static Sheets getSheetsService() throws IOException, GeneralSecurityException {
-        Credential credential = authorize();
-        return new Sheets.Builder(GoogleNetHttpTransport.newTrustedTransport(),
-                JacksonFactory.getDefaultInstance(), credential)
-                .setApplicationName(Config.APPLICATION_NAME)
-                .build();
-    }
-
     public static void writeToSheet(List<String> sentence, String sheetName) throws IOException, GeneralSecurityException {
-        sheetsService = Service.getSheetsService();
+        sheetsService = GoogleTools.getSheetsService();
 
         ValueRange appendBody = new ValueRange()
                 .setValues(Arrays.asList(new List[]{sentence}));
@@ -131,9 +92,7 @@ public class Service {
         return stringBuffer.toString();
     }
 
-    public static String cutCommandFromMessage(Update update) {
-        //todo implement this
-
+    private static String cutCommandFromMessage(Update update) {
         return update.getMessage().getText().substring(update.getMessage().getText().indexOf(" ") + 1);
     }
 
