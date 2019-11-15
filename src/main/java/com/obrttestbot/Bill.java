bedb6bc041b4bd123524fde2d10eb62833332a5b
Андрей Григорьев <greegav@gmail.com>
2019-11-15 18:00:50 +0200

    add somecode to Bill. Maybe convert to billDTO?

--- src/main/java/com/obrttestbot/Bill.java
+++ src/main/java/com/obrttestbot/Bill.java
@@ -1,6 +1,5 @@
 package com.obrttestbot;
 
-
 import java.util.Date;
 
 public class Bill {
@@ -14,11 +13,75 @@ public class Bill {
     private Date billDateValid;
 
     private Product product;
+    private int productQuantity;
 
     private double totalSumNumber;
     private String totalSumName;
 
     private String billAuthor;
 
+    public String getTotalSumName() {
+        return getPriceToString(this.totalSumNumber);
+    }
 
+    private String getPriceToString(double totalSumNumber) {
+        // TODO convert price to string
+        return "";
+    }
+
+    public void setBillAuthor(Firm sender) {
+        this.billAuthor = sender.getFirmHead();
+    }
+
+    public String getBillAuthor() {
+        return sender.getFirmHead();
+    }
+
+    public Firm getRecepient() {
+        return recepient;
+    }
+
+    public void setRecepient(Firm recepient) {
+        this.recepient = recepient;
+    }
+
+    public Firm getSender() {
+        return sender;
+    }
+
+    public void setSender(Firm sender) {
+        this.sender = sender;
+    }
+
+    public Firm getPayer() {
+        return payer;
+    }
+
+    public void setPayer(Firm payer) {
+        this.payer = payer;
+    }
+
+    public String getBillReason() {
+        return billReason;
+    }
+
+    public void setBillReason(String billReason) {
+        this.billReason = billReason;
+    }
+
+    public String getBillNumber() {
+        return billNumber;
+    }
+
+    public void setBillNumber(String billNumber) {
+        this.billNumber = billNumber;
+    }
+
+    public Date getBillDateCreate() {
+        return billDateCreate;
+    }
+
+    public void setBillDateCreate(Date billDateCreate) {
+        this.billDateCreate = billDateCreate;
+    }
 }
